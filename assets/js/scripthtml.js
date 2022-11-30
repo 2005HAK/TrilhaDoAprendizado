83470cc46c53084ff48b0916da2940f131ae4926
PCIF <instituto@ifpr.com>
2022-11-30 11:35:23 -0300

    alterações feitas em aula (detalhes quase prontos)

--- assets/js/scripthtml.js
+++ assets/js/scripthtml.js
@@ -4,11 +4,11 @@ function detalhes(n){
 
     for(var i=0; i < 11; i++){
         if(n==i){
-            detalhes[n].style.cssText = "visibility: visible;"+"display: flex;";
-            botao[n].style.cssText = "background-color: #b1b1b1;"+"top: .4em";
+            detalhes[n].style.cssText = "visibility: visible;"+"display: flex;"+"border-radius: 0 0 .5em .5em;";
+            botao[n].style.cssText = "background-color: #ebebeb;"+"border: none;"+"border-top: 1px solid black;"+"border-right: 1px solid black;"+"border-left: 1px solid black;"+"border-radius: .5em .5em 0 0;"+"top: 13px";
         } else {
             detalhes[i].style.cssText = "visibility: hidden;"+"display: none;";
             botao[i].style.cssText = "background-color: #000000;";
         }
-    }
+    }/*arrumar as bordas dos detalhes*/
 }
