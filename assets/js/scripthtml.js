fdcf858886fac950275aaeaec8e56b7c1dc453a1
2005HAK <hebertkubis15@gmail.com>
2022-12-05 22:28:18 -0300

    Bordas concertadas

--- assets/js/scripthtml.js
+++ assets/js/scripthtml.js
@@ -1,14 +1,20 @@
-function detalhes(n){
+function detalhes(n) {
     let detalhes = document.getElementsByClassName('detalhes');
     let botao = document.getElementsByClassName('botao');
 
-    for(var i=0; i < 11; i++){
-        if(n==i){
-            detalhes[n].style.cssText = "visibility: visible;"+"display: flex;"+"border-radius: 0 0 .5em .5em;";
-            botao[n].style.cssText = "background-color: #ebebeb;"+"border: none;"+"border-top: 1px solid black;"+"border-right: 1px solid black;"+"border-left: 1px solid black;"+"border-radius: .5em .5em 0 0;"+"top: 13px";
+    for (var i = 0; i < 11; i++) {
+        if (n == i) {
+            if (n == 0 || n == 5 || n == 10) {
+                detalhes[n].style.cssText = "visibility: visible;" + "display: flex;" + "border-radius: 0 .5em .5em .5em;";
+            } else if(n == 4 || n == 9) {
+                detalhes[n].style.cssText = "visibility: visible;" + "display: flex;" + "border-radius: .5em 0 .5em .5em;";
+            } else {
+                detalhes[n].style.cssText = "visibility: visible;" + "display: flex;" + "border-radius: .5em;";
+            }
+            botao[n].style.cssText = "text-shadow: none;"+"color: #000000;"+"background-color: #ebebeb;" + "border: none;" + "border-top: 1px solid black;" + "border-right: 1px solid black;" + "border-left: 1px solid black;" + "border-radius: .5em .5em 0 0;" + "top: 13px";
         } else {
-            detalhes[i].style.cssText = "visibility: hidden;"+"display: none;";
+            detalhes[i].style.cssText = "visibility: hidden;" + "display: none;";
             botao[i].style.cssText = "background-color: #000000;";
         }
-    }/*arrumar as bordas dos detalhes*/
+    }
 }
